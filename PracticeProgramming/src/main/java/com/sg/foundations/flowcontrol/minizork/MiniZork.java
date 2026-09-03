2902076489a24ac4f60324d0f7051fe37b5746c6
reiwallace <reiwallace@yahoo.com>
2026-09-03 17:07:46 +0100

    Add comments to minizork

--- PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/minizork/MiniZork.java
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/minizork/MiniZork.java
@@ -4,10 +4,8 @@ import com.sg.foundations.flowcontrol.minizork.location.AbstractLocation;
 import com.sg.foundations.flowcontrol.minizork.location.FarmHouse;
 import com.sg.foundations.flowcontrol.minizork.location.GrassyField;
 import com.sg.foundations.flowcontrol.minizork.location.OakTree;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import static com.sg.foundations.flowcontrol.minizork.location.LevelNames.*;
 
 public class MiniZork {
@@ -18,22 +16,34 @@ public class MiniZork {
     public static void main(String[] args) {
         setUpGameLocations();
         moveLocation(GRASSY_FIELD.name);
+
+        // Keep asking choices until the game is stopped
         while(running) {
             currentLocation.askChoices();
         }
     }
 
+    /**
+     * Loads game locations into the location map
+     */
     private static void setUpGameLocations() {
         locations.put(GRASSY_FIELD.name, new GrassyField());
         locations.put(OAK_TREE.name, new OakTree());
         locations.put(FARMHOUSE.name, new FarmHouse());
     }
 
+    /**
+     * Moves the player to another location
+     * @param name String name of the location
+     */
     public static void moveLocation(String name) {
         currentLocation = locations.get(name);
         currentLocation.enter();
     }
 
+    /**
+     * Stops the game loop
+     */
     public static void stopGame() {
         running = false;
     }
