d8900d8b7b6c85fcc3270a1e2a2be3e7ac38f048
reiwallace <reiwallace@yahoo.com>
2026-09-03 13:30:32 +0100

    Add quick println class

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/ifs/TriviaNight.java
@@ -0,0 +1,4 @@
+package com.sg.foundations.flowcontrol.ifs;
+
+public class TriviaNight {
+}
---
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/utils/QuickOut.java
@@ -0,0 +1,11 @@
+package com.sg.foundations.flowcontrol.utils;
+
+public class QuickOut {
+    /**
+     * Shorthand for System.out.println();
+     * @param line String input to print
+     */
+    public static void println(String line) {
+        System.out.println(line);
+    }
+}
