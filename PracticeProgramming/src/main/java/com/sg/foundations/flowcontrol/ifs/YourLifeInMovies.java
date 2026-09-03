21453d29437b11737e4a25a092e13517cdbf6650
reiwallace <reiwallace@yahoo.com>
2026-09-03 12:50:48 +0100

    Upload if tasks

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/ifs/YourLifeInMovies.java
@@ -0,0 +1,34 @@
+package com.sg.foundations.flowcontrol.ifs;
+
+import com.sg.foundations.flowcontrol.utils.UserInputUtils;
+import java.util.Scanner;
+
+public class YourLifeInMovies {
+    public static void main(String[] args) {
+        // Take user input
+        System.out.println("Hey, let's play a game! What's your name?");
+        String name = new Scanner(System.in).nextLine();
+
+        System.out.println("Ok, " + name + ", when were you born?");
+        int year = UserInputUtils.retrieveIntegerInput();
+
+        // Handle user input
+        // Print header in the first if statement since all below it will run if this one runs
+        if(year < 2005) {
+            System.out.println("Well " + name + "...");
+            System.out.println("Did you know that Pixar's 'Up' came out over a decade ago?");
+        }
+        if(year < 1995) {
+            System.out.println("The first Harry Potter came out over 15 years ago!");
+        }
+        if(year < 1985) {
+            System.out.println("Space Jam came out not last decade, but the one before THAT.");
+        }
+        if(year < 1975) {
+            System.out.println("The original Jurassic Park release is closer to the first lunar landing than it is to today.");
+        }
+        if(year < 1965) {
+            System.out.println("The MASH TV series has been around for almost half a century!");
+        }
+    }
+}
---
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/utils/UserInputUtils.java
@@ -0,0 +1,65 @@
+package com.sg.foundations.flowcontrol.utils;
+
+import java.util.Scanner;
+
+public class UserInputUtils {
+    private static final int MAX_INPUT_ATTEMPTS = 3;
+
+    /**
+     * Attempts to retrieve and parse integer input from System.In
+     * @return User inputted value
+     */
+    public static int retrieveIntegerInput() {
+        Scanner input = new Scanner(System.in);
+        String inputVal;
+
+        // Loop up to set number of attempts before returning 0
+        for(int i = 1; i <= MAX_INPUT_ATTEMPTS; i++) {
+            inputVal = input.nextLine();
+            // Attempt to parse the value - Inform user if fail
+            try {
+                int val = Integer.parseInt(inputVal);
+                if(val < 0) {
+                    System.out.println("Negative values not accepted. Please try again.");
+                    System.out.println("Attempts remaining: " + (MAX_INPUT_ATTEMPTS - i));
+                } else {
+                    return val;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number format (e.g. 0, 1, -5, 2000). Please try again.");
+                System.out.println("Attempts remaining: " + (MAX_INPUT_ATTEMPTS - i));
+            }
+        }
+        System.out.println("Max attempts exceeded.");
+        throw new RuntimeException();
+    }
+
+    /**
+     * Attempts to retrieve and parse Double input from System.In
+     * @return User inputted value
+     */
+    public static double retrieveDoubleInput() {
+        Scanner input = new Scanner(System.in);
+        String inputVal;
+
+        // Loop up to set number of attempts before returning 0
+        for(int i = 1; i <= MAX_INPUT_ATTEMPTS; i++) {
+            inputVal = input.nextLine();
+            // Attempt to parse the value - Inform user if fail
+            try {
+                double val = Double.parseDouble(inputVal);
+                if(val < 0) {
+                    System.out.println("Negative values not accepted. Please try again.");
+                    System.out.println("Attempts remaining: " + (MAX_INPUT_ATTEMPTS - i));
+                } else {
+                    return val;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number format (e.g. 0, 1.25, 2000.01). Please try again.");
+                System.out.println("Attempts remaining: " + (MAX_INPUT_ATTEMPTS - i));
+            }
+        }
+        System.out.println("Max attempts exceeded.");
+        throw new RuntimeException();
+    }
+}
