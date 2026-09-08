7bfead56d0ab56292bcd8ef23b8c4c24c654da37
reiwallace <reiwallace@yahoo.com>
2026-09-08 17:05:28 +0100

    Add classes from interface learning page

---
+++ PracticeProgramming/src/main/java/com/mthree/academy/c458/interfaces/learning/EmergencyVehicle.java
@@ -0,0 +1,8 @@
+package com.mthree.academy.c458.interfaces.learning;
+
+public interface EmergencyVehicle extends Vehicle {
+    void flashersOn();
+    void flashersOff();
+    void sirenOn();
+    void sirenOff();
+}
---
+++ PracticeProgramming/src/main/java/com/mthree/academy/c458/interfaces/learning/Trackable.java
@@ -0,0 +1,7 @@
+package com.mthree.academy.c458.interfaces.learning;
+
+public interface Trackable {
+    void sendCurrentLocation();
+    void beaconOn();
+    void beaconOff();
+}
