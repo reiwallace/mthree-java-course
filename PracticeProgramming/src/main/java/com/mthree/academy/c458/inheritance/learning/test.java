5a9e78b2c077e8c49b3857f8d21d7159b6ebacc3
reiwallace <reiwallace@yahoo.com>
2026-09-08 18:08:51 +0100

    Add classes from inheritance learning page

---
+++ PracticeProgramming/src/main/java/com/mthree/academy/c458/inheritance/learning/test.java
@@ -0,0 +1,8 @@
+package com.mthree.academy.c458.inheritance.learning;
+
+public class test {
+    public static void main(String[] args) {
+        Manager manager = new Manager();
+        manager.
+    }
+}
